5e261432c0efd5e31247d441408ca2fb9334cc33
Andrew Tian <113626415+Zeldoriz@users.noreply.github.com>
2024-05-03 12:12:09 +0700

    Update drawMap.js

--- public/js/drawMap.js
+++ public/js/drawMap.js
@@ -4,7 +4,7 @@ var map;
 var newShape = {};
 var checkShape = false;
 
-var prevLoc = {};
+var prevLoc = {lat: 0, lng: 0};
 var lat, lng;
 var marker;
 var markers = [];
@@ -27,15 +27,10 @@ function InitMap() {
   var all_overlays = [];
   var selectedShape;
   var drawingManager = new google.maps.drawing.DrawingManager({
-    // drawingMode: google.maps.drawing.OverlayType.MARKER,
-    // drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
       drawingModes: [
-        // google.maps.drawing.OverlayType.MARKER,
-        // google.maps.drawing.OverlayType.CIRCLE,
         google.maps.drawing.OverlayType.POLYGON,
-        // google.maps.drawing.OverlayType.RECTANGLE
       ],
     },
     markerOptions: {
